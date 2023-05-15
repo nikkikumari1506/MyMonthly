6d86ea704bd7338c66ed2589f76ada0fb1f760ff
Nikki Kumari <1805781@kiit.ac.in>
2023-05-16 00:33:08 +0530

    updating the links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
           <div className="row">
             <div className="col">
             <ul className="horizontal-list">
-            <li className="seperator">❤︎ Fork this on<a href="https://github.com/nikkikumari1506/"> Github</a></li>
+            <li className="seperator">❤︎ Fork this on<a href="https://github.com/nikkikumari1506/Mymonthly.git"> Github</a></li>
             
             
             </ul>
@@ -17,8 +17,7 @@ export default function Footer() {
         <hr />
         <div className="row">
             <p className = "col-sm">
-                &copy;{new Date().getFullYear()}・MyMonthly・All rights reserved・<a href="https://nikkikumari.notion.site/5c6e9c5d8aed42e79fc82a50b7bb8986
-">Nikki Kumari</a>
+                &copy;{new Date().getFullYear()}・MyMonthly・All rights reserved・<a href="https://kumarinikki.com">Nikki Kumari</a>
             </p>
         </div>
       </div>
